43bb4cb314d58013756391cfae3059eb1f5c64ff
David Lamhauge <davidlamhauge@gmail.com>
2023-07-02 19:37:17 +0200

    Filelist showing in dialog, when button is pressed

--- lib/send/send_task.dart
+++ lib/send/send_task.dart
@@ -10,44 +10,77 @@ class SendTask extends StatefulWidget {
 }
 
 class _SendTaskState extends State<SendTask> {
+
   String directory = '';
   List<String> strList = [];
   List<String> filNavne = [];
+  String alertText = '';
 
-  void filNavneList() {
-    String txt = '';
-    for (int i = 0; i < strList.length; i++) {
-      if (strList[i].endsWith('b7')) {
-        List<String> filNavn = strList[i].split('/');
-        filNavne.add(filNavn.last);
-      }
+  // this works!
+  void _createAlertText() {
+    alertText = '';
+    for (var txt in filNavne) {
+      List<String> tmp = txt.split('/');
+      alertText += '${tmp.last}\n';
     }
   }
 
+  // this works!
+  void filNavneList() {
+    filNavne.clear();
+    for (var txt in strList) {
+      if (txt.endsWith('b7')) {
+        filNavne.add('$txt\n');
+      }
+    }
+//    print('Filnavne: $filNavne Antal: ${filNavne.length}');
+  }
+
+  // this works
   Future<void> getDir() async {
+    strList.clear();
     final directory = await getApplicationDocumentsDirectory();
     await for (var entity
         in directory.list(recursive: true, followLinks: false)) {
       strList.add(entity.path);
     }
+//    print('dir: $strList Antal: ${strList.length}');
+  }
+
+  void _lateInit() async {
+    await getDir();
+    filNavneList();
+    _createAlertText();
   }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    getDir();
-    filNavneList();
   }
 
   @override
   Widget build(BuildContext context) {
+    _lateInit();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Send opgave'),
       ),
-      body: const Column(
-        children: [],
+      body: Column(
+        children: [
+          ElevatedButton(
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (context){
+                      return AlertDialog(
+                        content: Text(alertText),
+                      );
+                    });
+              },
+              child: const Text('Hent filliste'),
+          )
+        ],
       ),
     );
   }
